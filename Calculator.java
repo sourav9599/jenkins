bb553b41fbac2bb9c13a187c44586a37b3be20d4
Sourav Mohanty <sourav10mohanty@gmail.com>
2021-08-21 13:19:54 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String args[]) {
+      int x=Integer.parseInt(args[0]);
+      int y=Integer.parseInt(args[1]);
+      System.out.println("Multiplication of x and y = " + (x * y));
+      System.out.println("Division of x and y = " + (x / y));
+
+    }
+}
